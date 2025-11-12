b36da14e68df5e83fce72b6770241bd3a7bbaaaa
Ivan Serebrennikov <admin@silberworks.com>
2025-11-12 23:26:40 +0300

    feat(version): set

--- pepeunit.go
+++ pepeunit.go
@@ -3,7 +3,7 @@ package pepeunit
 import "time"
 
 // Version represents the library version
-const Version = "0.1.0"
+const Version = "0.10.1"
 
 // DefaultCycleSpeed is the default cycle speed for the main loop
 const DefaultCycleSpeed = 100 * time.Millisecond
